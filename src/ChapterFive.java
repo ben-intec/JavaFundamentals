03723525fcaf2b342893a358603fba6046e21749
bny-mobile <bny.mobile@gmail.com>
2021-03-09 11:29:06 +0100

    seperated the exercises

--- src/ChapterFive.java
+++ src/ChapterFive.java
@@ -4,17 +4,33 @@ public class ChapterFive {
 
     public static void main(String[] args) {
 
-        System.out.println("Hoe oud ben je?");
         var scanner = new Scanner(System.in);
+
+        getAge(scanner);
+
+        Utils.pause(2000);
+
+        getBMI(scanner);
+
+        scanner.close();
+    }
+
+    //Opdracht 14 a
+    public static void getAge(Scanner scanner) {
+        System.out.println("Hoe oud ben je?");
+
         var age = scanner.nextInt();
         var formatAge = "je bent een %s%n";
+
         if (age<2) System.out.printf(formatAge,"baby");
         else if (age<10) System.out.printf(formatAge,"kind");
         else if (age<20) System.out.printf(formatAge,"tiener");
         else System.out.printf(formatAge,"Volwassene");
 
-        Utils.pause(1000);
+    }
 
+    //Opdracht 14 b
+    public static void getBMI(Scanner scanner) {
         System.out.println("Hoeveel weeg je? (in kg)");
         var weight = scanner.nextFloat();
 
@@ -29,7 +45,5 @@ public class ChapterFive {
         else if(bmi<30) System.out.printf(formatBmi, bmi, "overgewicht!");
         else if(bmi<40) System.out.printf(formatBmi, bmi, "obesitas!");
         else System.out.printf(formatBmi, bmi, "ziekelijk overgewicht!");
-
-        scanner.close();
     }
 }
