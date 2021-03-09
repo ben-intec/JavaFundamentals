54d3737d8dd584d0330ed5cb702b7bb4f222b6fc
bny-mobile <bny.mobile@gmail.com>
2021-03-09 13:46:03 +0100

    code clean up

--- src/ChapterFive.java
+++ src/ChapterFive.java
@@ -17,33 +17,52 @@ public class ChapterFive {
 
     //Opdracht 14 a
     public static void getAge(Scanner scanner) {
+
+        //get user age
         System.out.println("Hoe oud ben je?");
 
         var age = scanner.nextInt();
-        var formatAge = "je bent een %s%n";
 
-        if (age<2) System.out.printf(formatAge,"baby");
-        else if (age<10) System.out.printf(formatAge,"kind");
-        else if (age<20) System.out.printf(formatAge,"tiener");
-        else System.out.printf(formatAge,"Volwassene");
+        //inform user
+        var format = "je bent een %s%n";
+
+        if (age<2)
+            System.out.printf(format,"baby");
+        else if (age<10)
+            System.out.printf(format,"kind");
+        else if (age<20)
+            System.out.printf(format,"tiener");
+        else
+            System.out.printf(format,"Volwassene");
 
     }
 
     //Opdracht 14 b
     public static void getBMI(Scanner scanner) {
+        //get user weight
         System.out.println("Hoeveel weeg je? (in kg)");
         var weight = scanner.nextFloat();
 
+        //get user height
         System.out.println("Hoe groot ben je? (in m)");
         var height = scanner.nextFloat();
 
+        //calculate user BMI
         var bmi = weight / (height * height);
-        var formatBmi = "met een BMI van %.2f heb je %s%n";
 
-        if(bmi<20) System.out.printf(formatBmi, bmi, "ondergewicht!");
-        else if(bmi<25) System.out.printf(formatBmi, bmi, "een normaal gewicht!");
-        else if(bmi<30) System.out.printf(formatBmi, bmi, "overgewicht!");
-        else if(bmi<40) System.out.printf(formatBmi, bmi, "obesitas!");
-        else System.out.printf(formatBmi, bmi, "ziekelijk overgewicht!");
+        //Inform user
+        var format = "met een BMI van %.2f heb je %s%n";
+
+        if(bmi<20)
+            System.out.printf(format, bmi, "ondergewicht!");
+        else if(bmi<25)
+            System.out.printf(format, bmi, "een normaal gewicht!");
+        else if(bmi<30)
+            System.out.printf(format, bmi, "overgewicht!");
+        else if(bmi<40)
+            System.out.printf(format, bmi, "obesitas!");
+        else
+            System.out.printf(format, bmi, "ziekelijk overgewicht!");
+
     }
 }
