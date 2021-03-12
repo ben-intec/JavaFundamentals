8d849789363bb8b2055bcd1bcbe10c326b5c1aad
bnymobile <bny.mobile@gmail.com>
2021-03-12 17:30:35 +0100

    Implemented prime checker

---
+++ src/Chapter5/Methods.java
@@ -0,0 +1,62 @@
+package Chapter5;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Scanner;
+
+public class Methods {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        int range = getRangeMaximum(scanner);
+
+        checkRangeForPrimes(range);
+
+        scanner.close();
+    }
+
+    public static void checkRangeForPrimes(int range) {
+
+        int count = 0;
+        for (int i = 0; i <= range; i+= 2) {
+            if(isPrime(i)) {
+                System.out.println(i);
+                count++;
+            }
+
+            if(i == 2) i--; //-- only check uneven numbers larger than 2
+        }
+
+        System.out.println(count + " primes found within range!");
+    }
+
+    public static boolean isPrime(int number) {
+
+        for (int i = 2; i <= number/2; i++)
+            if (number % i == 0)
+                return false;
+
+        return number > 1;
+    }
+
+    public static int getRangeMaximum(@NotNull Scanner scanner) {
+
+        int k ;
+        System.out.println("Please input top range for prime query:");
+
+        while(!scanner.hasNextInt()) {
+            String input = scanner.next();
+            System.out.printf("\"%s\" is not a valid number.%n", input);
+        }
+
+        k = scanner.nextInt();
+
+        while (k < 0) {
+            k = scanner.nextInt();
+            System.out.printf("%d is not a valid number. Only Whole Numbers are allowed%n", k);
+        }
+
+
+        return k;
+    }
+}
