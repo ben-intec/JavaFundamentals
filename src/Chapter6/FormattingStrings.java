0dc86141387971c357df667bc5425bb09b789da5
bnymobile <bny.mobile@gmail.com>
2021-03-16 15:39:14 +0100

    Opdracht 5 Formatter

---
+++ src/Chapter6/FormattingStrings.java
@@ -0,0 +1,10 @@
+package Chapter6;
+//Opdracht 5
+public class FormattingStrings {
+    public static void main(String[] args) {
+
+        for (int i = 100; i < 2001; i+=50) {
+            System.out.printf("%.2f m\t  =  %.2f ft%n", i/100f, i * 0.03280839895);
+        }
+    }
+}
